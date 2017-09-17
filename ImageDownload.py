890acbb2979f5c56abbb89a667b655ac5d975d49
T AKSHAYA KUMAR <takshaya.kumar.civ13@iitbhu.ac.in>
2017-09-17 21:57:21 +0530

    Update ImageDownload.py

--- ImageDownload.py
+++ ImageDownload.py
@@ -1,2 +1,2 @@
 import urllib
-urllib.urlretrieve("PUT IMAGE ADDRESS HERE","C:\Python27_project\image\file_name.jpg")
+urllib.urlretrieve("PUT IMAGE ADDRESS HERE!","C:\Python27_project\image\file_name.jpg")
